9b40b004e996bdd8a0caaebf70d7b08abbd65c32
Shubham Shinde <shubhamshinde225@gmail.com>
2024-07-21 14:41:57 +0530

    New Project created for rest to soap converter

---
+++ src/main/java/com/connector/resttosoap/ResttosoapApplication.java
@@ -0,0 +1,14 @@
+package com.connector.resttosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttosoapApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ResttosoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connector/resttosoap/ResttosoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connector.resttosoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttosoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
